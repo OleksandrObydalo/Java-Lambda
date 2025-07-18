de3e5fb10c4c994cb443666de7537349b770310e
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-18 12:53:04 +0300

    Add Player class

---
+++ src/main/java/lambdas/LambdasMain.java
@@ -0,0 +1,15 @@
+package lambdas;
+
+public class LambdasMain {
+    public static void main(String[] args) {
+        String[] colours = {"red", "orange", "yellow", "green", "blue"};
+
+        Player player1 = new Player("Alex", 100);
+        Player player2 = new Player("Igor", 80);
+        Player player3 = new Player("Victor", 85);
+        Player player4 = new Player("John", 91);
+
+        Player[] players = {player1, player2, player3, player4};
+
+    }
+}
