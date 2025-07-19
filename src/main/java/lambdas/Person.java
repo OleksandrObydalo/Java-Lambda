968ae4c3d55c175f9d629247cb8a97c13df33360
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-19 20:39:45 +0300

    Add Person class

---
+++ src/main/java/lambdas/Person.java
@@ -0,0 +1,37 @@
+package lambdas;
+
+public class Person {
+    protected String firstName;
+    protected String lastName;
+    protected double salary;
+
+    public Person(String firstName, String lastName, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
