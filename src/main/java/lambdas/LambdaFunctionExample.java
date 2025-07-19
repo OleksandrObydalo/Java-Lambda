6293625bf1e5efff2dac1774c4d2ddc06651e979
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-19 20:48:27 +0300

    Create people and employees list

--- src/main/java/lambdas/LambdaFunctionExample.java
+++ src/main/java/lambdas/LambdaFunctionExample.java
@@ -1,7 +1,20 @@
 package lambdas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LambdaFunctionExample {
     public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Alex", "Black", 25, 50000));
+        employees.add(new Employee("John", "Green", 30, 75000));
+        employees.add(new Employee("Sam", "Brown", 27, 80000));
+        employees.add(new Employee("Tony", "Grey", 23, 90000));
 
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alex", "Smith", 25));
+        people.add(new Person("John", "Green", 30));
+        people.add(new Person("Jam", "Brown", 32));
+        people.add(new Person("Nicola", "Tesla", 34));
     }
 }
