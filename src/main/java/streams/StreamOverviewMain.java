1975a33cee09996e0af63c8ca99ced4b1838d77f
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-21 20:14:08 +0300

    Create stream to write first name for each Employee feom each department (Employee list)

--- src/main/java/streams/StreamOverviewMain.java
+++ src/main/java/streams/StreamOverviewMain.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 
 public class StreamOverviewMain {
     static List<Employee>  employeeList = new ArrayList<>();
+    static List<Employee>  secondList = new ArrayList<>();
     static Map<Integer, Employee> employeeMap = null;
 
     public static void main(String[] args) {
@@ -25,34 +26,65 @@ public class StreamOverviewMain {
         employeeList.add(new Employee(10, "Mike", "Yellow", 42, 60000));
         employeeList.add(new Employee(11, "Victoria", "Pink", 32, 75000));
 
-//        testStreamFromList();
-        testStreamFromFile();
+        secondList.add(new Employee(1, "Alex", "Black", 30, 50000));
+        secondList.add(new Employee(2, "John", "Green", 35, 75000));
+        secondList.add(new Employee(6, "Sam", "Brown", 27, 80000));
+        secondList.add(new Employee(9, "Tony", "Grey", 36, 90000));
+        secondList.add(new Employee(10, "Mike", "Yellow", 23,  60000));
+        secondList.add(new Employee(11, "Victoria", "Pink", 21, 75000));
+        secondList.add(new Employee(16, "Sean", "Magenta", 28, 80000));
+        secondList.add(new Employee(19, "Kate", "Black", 34, 88000));
+        secondList.add(new Employee(9, "Tony", "Grey", 40, 90000));
+        secondList.add(new Employee(10, "Mike", "Yellow", 42, 60000));
+        secondList.add(new Employee(11, "Victoria", "Pink", 32, 75000));
+
+
+        testStreamFromList();
+//        testStreamFromFile();
     }
 
     private static void testStreamFromList() {
-        employeeList.stream()
-                .filter(e -> e.getSalary() > 60000)
-                .filter(e -> e.getId() > 10)
-                .forEach(System.out::println);
-
+//        employeeList.stream()
+//                .filter(e -> e.getSalary() > 60000)
+//                .filter(e -> e.getId() > 10)
+//                .forEach(System.out::println);
+//
         Integer[] ids = new Integer[20];
         for(int i = 0; i < ids.length; i++){
             ids[i] = i + 1;
         }
+//
+//        Stream.of(ids)
+//                .map(StreamOverviewMain::findById)
+//                .filter(Objects::nonNull)
+//                .collect(Collectors.toList())
+//                .forEach(System.out::println);
+//
+//        Optional<Employee> first = Stream.of(ids)
+//                .map(StreamOverviewMain::findById)
+//                .filter(Objects::nonNull)
+//                .findFirst();
+//
+//        System.out.println("first = " + first);
+//        System.out.println("first.get() =" +first.get());
 
-        Stream.of(ids)
+        int sum = Stream.of(ids)
                 .map(StreamOverviewMain::findById)
                 .filter(Objects::nonNull)
-                .collect(Collectors.toList())
+                .mapToInt(Employee::getSalary)
+                .sum();
+
+        System.out.println("Sum of salaries = " + sum);
+
+        List<List<Employee>> departments = new ArrayList<>();
+        departments.add(employeeList);
+        departments.add(secondList);
+
+        departments.stream().flatMap(l->l.stream().map(e->e.getFirstName())).forEach(System.out::println);
+        Stream.of(ids)
+                .peek(e->e=e*2)
                 .forEach(System.out::println);
 
-        Optional<Employee> first = Stream.of(ids)
-                .map(StreamOverviewMain::findById)
-                .filter(Objects::nonNull)
-                .findFirst();
-
-        System.out.println("first = " + first);
-        System.out.println("first.get() =" +first.get());
     }
 
     private static void testStreamFromFile() {
